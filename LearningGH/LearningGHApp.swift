c2d9966298c94f95c7f9d0044d763c8f71ae0161
Scott Willis <scott.willis76@gmail.com>
2022-07-28 10:29:11 +0100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Scott Willis on 28/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
